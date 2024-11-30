87eb7d4304db8a6d8b45a698f8831a0b4eb5cad5
FauziahAzura <fauziahazura068@gmail.com>
2024-11-30 22:00:09 +0700

    DDP10.py

---
+++ call.py
@@ -0,0 +1,15 @@
+import bangundatar
+
+bangundatar.l_persegi(5)
+bangundatar.l_persegi_panjang(10,5)
+bangundatar.l_segitiga(6,9)
+bangundatar.l_lingkaran(4)
+bangundatar.l_jajar_genjang(2,4)
+
+import bangunruang
+
+bangunruang.l_balok(4,9,2)
+bangunruang.l_kubus(8)
+bangunruang.l_tabung(6,4)
+bangunruang.l_limas_segitiga(6,4,8,3,2)
+bangunruang.l_prisma_segitiga(4,2,1,2,6,5)
