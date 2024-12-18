050b65c6ebc9fea3191e299ce0e9a57cbe789379
FauziahAzura <fauziahazura068@gmail.com>
2024-12-18 11:09:14 +0700

    Pertemuan12.

---
+++ pratikum12/Kucing.py
@@ -0,0 +1,19 @@
+from Animal import Animal
+
+class Kucing (Animal):
+    def __init__(self, nama, makanan, hidup, berkembang_biak, Negara_asal, warna):
+        super().__init__(nama, makanan, hidup, berkembang_biak)
+        self.Negara_asal = Negara_asal
+        self.warna = warna
+    def cetak_Kucing(self):
+        super().info_animal()
+        print("Negara Asal\t\t: ", self.Negara_asal,
+              "\nWarna \t\t\t: ", self.warna)
+Persia = Kucing("Persia", "Pur", "Darat", "Melahirkan", "Iran", "Coklat kekuningan")
+Persia.cetak_Kucing()
+print("================================================================")
+Siberia = Kucing("Siberia", "Pur", "Darat", "Melahirkan", "Rusia", "Cream & Hitam")
+Siberia.cetak_Kucing()
+print("================================================================")
+Scottish_Fold = Kucing("Scottish Fold", "Pur", "Darat", "Melahirkan", "Skotlandia", "Putih,Abu & Cream")
+Scottish_Fold.cetak_Kucing()
